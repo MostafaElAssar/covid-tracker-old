5fd20a8254cff6d05c82b591f2e11856aa2fb194
Mostafa El-Assar <moustafa.elassar@gmail.com>
2021-05-23 14:01:26 +0200

    Add breadcrumbs

--- src/components/BaseLayout/BaseLayout.style.ts
+++ src/components/BaseLayout/BaseLayout.style.ts
@@ -1,16 +1,20 @@
 import { css } from 'styled-components';
 
 export default css`
-  >.ant-layout{
-    margin-left: 250px; 
+  > .ant-layout {
+    margin-left: 250px;
     height: 100%;
     background: #fff;
     .ant-layout-content {
       height: calc(100vh - 188px);
-      >div {
-        text-align: center; 
+      > div {
+        text-align: center;
         border: 4px solid #f0f2f5;
-        height: 100%
+        height: 100%;
+        .ant-breadcrumb {
+          text-align: left;
+          padding: 12px;
+        }
       }
     }
   }
--- src/components/BaseLayout/BaseLayout.tsx
+++ src/components/BaseLayout/BaseLayout.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
+import { Breadcrumb, Layout } from 'antd';
 import styled from 'styled-components';
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import style from './BaseLayout.style';
 
-interface BaseLayoutProps {
+interface BaseLayoutProps extends RouteComponentProps {
   children: any;
   className?: string;
 }
@@ -14,19 +15,30 @@ interface BaseLayoutProps {
 const BaseLayout: React.FC<BaseLayoutProps> = ({
   children,
   className,
+  location,
 }: BaseLayoutProps) => (
   <Layout className={className}>
     <Sidebar />
     <Layout>
       <Header />
       <Layout.Content>
-        <div>{children}</div>
+        <div>
+          <Breadcrumb>
+            <Breadcrumb.Item>
+              {location.pathname.split('/')[1] === 'dashboard'
+                ? 'Dashboard'
+                : 'Home'}
+            </Breadcrumb.Item>
+            <Breadcrumb.Item> </Breadcrumb.Item>
+          </Breadcrumb>
+          {children}
+        </div>
       </Layout.Content>
       <Footer />
     </Layout>
   </Layout>
 );
 
-export default styled(BaseLayout)`
+export default styled(withRouter(BaseLayout))`
   ${style}
 `;
